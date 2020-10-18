e856947d902bfaa57d79d8becda5ae2af2072fdf
Ghiffari Assamar <travish90@gmail.com>
2020-10-19 00:18:04 +0700

    Initial Release

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 Route::group(['middleware' => 'web'], function () {
-	Route::view("ui/test","LaraXendit::test");
-	Route::view("ui","LaraXendit::order-detail");
-	Route::view("ui/payment","LaraXendit::payment");
-	Route::view("ui/retail-outlet","LaraXendit::retail-outlet");
-	Route::view("ui/virtual-account","LaraXendit::retail-outlet");
+	Route::view("laraxendit","LaraXendit::order-detail");
+	Route::view("laraxendit/payment","LaraXendit::payment");
+	Route::view("laraxendit/retail-outlet","LaraXendit::retail-outlet");
+	Route::view("laraxendit/virtual-account","LaraXendit::retail-outlet");
 });
